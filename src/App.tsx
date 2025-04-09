6cc0c613c1f7c7267af5b9e31b00deb262c6f90a
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2025-04-09 17:50:13 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RecipePage from "./pages/RecipePage";
-import RecipeDetails from "./pages/RecipeDetails";
 import Login from "./pages/LoginPage.tsx";
 import Signup from "./pages/SignupPage.tsx";
-import CreateRecipe from "./pages/CreateRecipe";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage.tsx";
 
@@ -13,11 +10,9 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<MainPage />} />
-                <Route path="/recipes" element={<RecipePage />} />
-                <Route path="/recipes/:id" element={<RecipeDetails />} />
+
                 <Route path="/login" element={<Login  />}/>
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/create-recipe" element={<CreateRecipe />} />
             </Routes>
             <Footer />
         </BrowserRouter>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     return (
         <section id="home" className="relative w-full h-[90vh] flex items-center justify-center text-center px-4">
             <div className="w-full h-full rounded-4xl overflow-hidden bg-cover bg-center relative"
-                 style={{ backgroundImage: "url('src/assets/newRecipe.jpg')" }}>
+                 style={{ backgroundImage: "url('src/assets/mainRecipe.jpg')" }}>
                 <div className="absolute inset-0 bg-black/40"></div>
                 <div className="relative flex flex-col items-center justify-center h-full text-white text-center pt-24">
                     <h2 className="text-5xl md:text-6xl font-extrabold drop-shadow-lg">Welcome to <span className="text-orange-400">TastyBites</span></h2>
